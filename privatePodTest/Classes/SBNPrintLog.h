c4debd26559a06043a4289527a6db9445c28314d
jinkai <kaijin@sohu-inc.com>
2017-02-27 11:32:14 +0800

    second commit

---
+++ privatePodTest/Classes/SBNPrintLog.h
@@ -0,0 +1,15 @@
+//
+//  SBNPrintLog.h
+//  
+//
+//  Created by jinkai on 17/2/27.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBNPrintLog : NSObject
+
+- (void)printHello;
+
+@end
---
+++ privatePodTest/Classes/SBNPrintLog.m
@@ -0,0 +1,17 @@
+//
+//  SBNPrintLog.m
+//  
+//
+//  Created by jinkai on 17/2/27.
+//
+//
+
+#import "SBNPrintLog.h"
+
+@implementation SBNPrintLog
+
+- (void)printHello{
+    NSLog(@"hello Pod");
+}
+
+@end
